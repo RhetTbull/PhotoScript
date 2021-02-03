35fa46f0b08ff508dfbd076c93069026ccfbed9a
Rhet Turnbull <rturnbull+git@gmail.com>
2021-02-03 06:38:34 -0800

    Updated doc theme

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -60,7 +60,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 # ---sphinx-themes-----
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
